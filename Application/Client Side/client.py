ac2e8ce6ff136b3ec3f7fdfcd612244160bbd199
PingaLCS <sgclau@liverpool.ac.uk>
2023-03-10 22:27:54 +0800

    code review for client and server side but the send file function is not fully done

--- Application/Client Side/client.py
+++ Application/Client Side/client.py
@@ -2,9 +2,14 @@ import socket
 import pickle
 import json
 import xml.etree.ElementTree as ET
+from cryptography.fernet import Fernet
 
 HOST = 'localhost'
 PORT = 5000
+
+# Set the key for encryption and decryption
+key = "NoPNbKq2NPZI4iFIhFs9uSXVAvBkQEGYZvGB_LmNgbA="
+#key = Fernet.generate_key()
 
 def create_dict():
     my_dict = {}
@@ -18,7 +23,7 @@ def serialize_dict(my_dict):
     if format == 'binary':
         return pickle.dumps(my_dict)
     elif format == 'JSON':
-        return json.dumps(my_dict)
+        return json.dumps(my_dict).encode('utf-8')
     elif format == 'XML':
         root = ET.Element("root")
         for key, value in my_dict.items():
@@ -32,33 +37,45 @@ def create_file():
         content = input("Enter file content: ")
         encrypt = input("Encrypt file content? (y/n): ")
         if encrypt == 'y':
-            content = encrypt_file(content)
+            # Create Fernet object with key
+            cipher_suite = Fernet(key)
+            # Encrypt file content
+            content = cipher_suite.encrypt(content.encode('utf-8')).decode('utf-8')
         with open(filename, 'w') as f:
             f.write(content)
+        return content
     except IOError as e:
         print(f"Failed to create file: {e}")
 
-def encrypt_file(content):
-    # Implement encryption algorithm here
-    return content
-
-def send_data(data):
+def send_data(serialized_data):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((HOST, PORT))
-            s.sendall(data)
+            s.sendall("1")
+            s.sendall(serialized_data)
+    except ConnectionRefusedError:
+        print("Connection refused. Server may not be running.")
+
+def send_file(serialized_data):
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            #s.sendall(key)
+            s.sendall("2")
+            s.sendall(serialized_data)
     except ConnectionRefusedError:
         print("Connection refused. Server may not be running.")
 
 def main():
     try:
-        choice = input("Enter choice (1: create dictionary, 2: create file): ")
+        choice = input("Enter choice (1: create dictionary, 2: create file) and send to server: ")
         if choice == '1':
             my_dict = create_dict()
             serialized_data = serialize_dict(my_dict)
             send_data(serialized_data)
         elif choice == '2':
-            create_file()
+            my_file = create_file()
+            send_file(my_file)
     except Exception as e:
         print(f"An error occurred: {e}")
 
